0719aec3e926fbabb7be428022a0957702be42f6
Nolan <nolanwinsman@gmail.com>
2022-05-19 18:47:03 -0500

    added many comments

--- movie_renamer.py
+++ movie_renamer.py
@@ -3,6 +3,10 @@ import os
 import sys
 
 def has_year(file):
+    """If the file contains four digits inside parenthesis,
+       returns True
+       otherwise, returns False
+    """
     count = 0
     parenthesis = {'(', ')'}
     for c in file:
@@ -12,10 +16,12 @@ def has_year(file):
             count = 0
         if count == 6:
             return True
-        # print(f"{c} : {count}")
     return False
 
 def text_after_year(file):
+    """Deletes all text after four digits inside parenthesis
+       Batman (1989) 1080.mkv   --->  Batman (1989).mkv  
+    """
     suffix = file[len(file)-4:]
     count = 0
     parenthesis = {'(', ')'}
@@ -26,11 +32,14 @@ def text_after_year(file):
             count = 0
         if count == 6:
             return file[:i+1] + suffix
-        # print(f"{c} : {count}")
     return file
 
 
 def parenthesis_year(file):
+    """If the string file has four numbers in a row representing a year,
+       adds parenthesis around the four numbers.
+       Batman 1989   ---->   Batman (1989)
+    """
     if has_year(file) == False:
         year = ''
         for c in file:
@@ -44,6 +53,8 @@ def parenthesis_year(file):
         return file
 
 def remove_periods(file):
+    """Returns the string file with all periods removed
+    """
     suffix = file[len(file)-4:]
     return file[:-4].replace(".", " ") + suffix
 
@@ -53,46 +64,38 @@ def main():
         print("No Directory given in argument")
         exit()
     rename_all = False
+    # if user inputs -y as the third argument
+    # does not prompt user 
     if len(sys.argv) >= 3:
         if sys.argv[2] == "-y":
             rename_all = True
-    # directory = os.getcwd()
-    # print(str(sys.argv[1]))
     directory = str(sys.argv[1])
+    # loops through all files directories and subdirectories
     for subdir, dirs, files in os.walk(directory):
         for file in files:
-            if file.endswith(".txt") and "RARBG" in str(file):
+            # deletes .txt and .exe files with "RARBG" in the filename
+            if (file.endswith(".txt") or file.endswith(".exe")) and "RARBG" in str(file):
                 print(f"Deleting file {file}")
-                txt_to_delete = os.path.join(subdir, file)
-                os.remove(txt_to_delete)
-                continue
-            if file.endswith(".exe") and "RARBG" in str(file):
-                print(f"Deleting file {file}")
-                exe_to_delete = os.path.join(subdir, file)
-                os.remove(exe_to_delete)
+                os.remove(os.path.join(subdir, file))
                 continue
             if file.endswith(".mp4") or file.endswith(".mkv"):
-                # print(file)
-                temp = parenthesis_year(file) 
-                # print(temp)
-                temp = remove_periods(temp)
-                # print(temp)
-                temp = text_after_year(temp)
+                temp = parenthesis_year(file) # puts parenthesis around year
+                temp = remove_periods(temp) # replaces periods with spaces
+                temp = text_after_year(temp) # deletes all text after the year
                 print('------------------------------')
                 print(f"Rename:\n{file}\n{temp}")
                 if rename_all:
                     response = "y"
                 else:
+                    print("-y for yes, anything else for no")
                     response = input()
                 if response == "y":
                     print("Renaming File")
                     old_file = os.path.join(subdir, file)
                     new_file = os.path.join(subdir, temp)
                     os.rename(old_file, new_file)
-
                 else:
                     print("Skipping File")
-                # print(os.path.join(subdir, file))
 
 
 if __name__ == "__main__":
