f652c87f7f365b83aeb2e8f40a54210aa05cadc6
Nolan <nolanwinsman@gmail.com>
2022-05-22 19:32:56 -0500

    subtitle automation started
    Can automatically handle some subtitle situations

--- movie_renamer.py
+++ movie_renamer.py
@@ -2,6 +2,16 @@
 import os
 import sys
 
+
+# Global variables
+DIRECTORY = str(sys.argv[1])
+RENAME_ALL = False
+
+def echo_ne(before, after):
+    print(f"\n------------------------------------------------------------------------------------------")
+    print(f"\t{before}\n\t{after}")
+    print(f"------------------------------------------------------------------------------------------\n")
+
 def text_after_year_folder(folder):
     """If the string file has four numbers in a row representing a year,
        adds parenthesis around the four numbers.
@@ -77,54 +87,71 @@ def remove_periods(file):
     suffix = file[len(file)-4:]
     return file[:-4].replace(".", " ") + suffix
 
-
-def main():
-    if len(sys.argv) < 2:
-        print("No Directory given in argument")
-        exit()
-    rename_all = False
-    # if user inputs -y as the third argument
-    # does not prompt user 
-    if len(sys.argv) >= 3:
-        if sys.argv[2] == "-y":
-            rename_all = True
-    directory = str(sys.argv[1])
-
-    for subdir, dirs, files in os.walk(directory):
-        if os.path.isdir(subdir):
-            if any((fname.endswith('.mp4') or fname.endswith('.mkv')) for fname in os.listdir(subdir)):
-                temp = remove_periods(subdir) # replaces periods with spaces
-                temp = text_after_year_folder(temp) # deletes all text after the year, including the year
-                print(f"\n{subdir}\n {temp}\n")
-    exit()
-
-
+def fix_movie_file():
     # loops through all files directories and subdirectories
-    for subdir, dirs, files in os.walk(directory):
+    for subdir, dirs, files in os.walk(DIRECTORY):
         for file in files:
             # deletes .txt and .exe files with "RARBG" in the filename
             if (file.endswith(".txt") or file.endswith(".exe")) and "RARBG" in str(file):
                 print(f"Deleting file {file}")
                 os.remove(os.path.join(subdir, file))
                 continue
+            
+            if (file.endswith(".nfo") or file.endswith(".idx") or file.endswith(".sub")):
+                print(f"Deleting {file}")
+                os.remove(os.path.join(subdir, file))
+
             if file.endswith(".mp4") or file.endswith(".mkv"):
                 temp = parenthesis_year(file) # puts parenthesis around year
                 temp = remove_periods(temp) # replaces periods with spaces
                 temp = text_after_year(temp) # deletes all text after the year
-                print('------------------------------')
-                print(f"Rename:\n{file}\n{temp}")
-                if rename_all:
-                    response = "y"
-                else:
-                    print("-y for yes, anything else for no")
-                    response = input()
-                if response == "y":
-                    print("Renaming File")
-                    old_file = os.path.join(subdir, file)
-                    new_file = os.path.join(subdir, temp)
-                    os.rename(old_file, new_file)
-                else:
-                    print("Skipping File")
+                old_name = os.path.join(subdir, file)
+                new_name = os.path.join(subdir, temp)
+                if old_name != new_name:
+                    echo_ne(file, temp)
+                    os.rename(old_name, new_name)
+
+def fix_movie_folder():
+    # loop for folders
+    for subdir, dirs, files in os.walk(DIRECTORY):
+        if os.path.isdir(subdir):
+            if any((fname.endswith('.mp4') or fname.endswith('.mkv')) for fname in os.listdir(subdir)):
+                temp = remove_periods(subdir) # replaces periods with spaces
+                temp = text_after_year_folder(temp) # deletes all text after the year, including the year
+                if temp != subdir:
+                    echo_ne(subdir, temp)
+                    os.rename(subdir, temp)
+
+def subtitles():
+    for subdir, dirs, files in os.walk(DIRECTORY):
+        for fname in files:
+            if fname.endswith('.srt'):
+                parent_parent = os.path.dirname(subdir)
+                movie_name = ""
+                if "subs" in str(subdir).lower():
+                    count = len([x for x in os.listdir(subdir)])
+                    if count == 1:
+                        for other_fname in os.listdir(parent_parent):
+                            if other_fname.endswith(".mp4") or other_fname.endswith(".mkv"):
+                                movie_name = other_fname
+                                new_name = f"{movie_name[:-4]}.en.srt"
+                                print(f"renaming {fname} to {new_name}")
+                                os.rename(os.path.join(subdir, fname), os.path.join(subdir, new_name))
+                                break
+
+
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("No Directory given in argument")
+        exit()
+    
+    print("Renaming Folders and Files")
+    fix_movie_file()
+    fix_movie_folder()
+    subtitles()
+    print("Completed")
 
 
 
