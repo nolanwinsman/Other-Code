b2a3f180f56efb26a523e1e8f3055e0477d48682
Nolan <nolanwinsman@gmail.com>
2022-06-20 18:05:51 -0600

    basic functionality complete

--- movie_renamer2.py
+++ movie_renamer2.py
@@ -1,6 +1,6 @@
 import os
 import sys
-import pathlib
+from pathlib import Path
 import imdb #run 'pip install IMDbPY' if you do not have the api
 
 # GLOBAL VARIABLES
@@ -25,6 +25,22 @@ class movie_struct():
     def print(self):
         print(f'KEY : {self.key}\nTITLE : {self.title}\nYEAR : {self.year}')
         print("---------------")
+    def rename(self):
+        if len(self.new_file_name) > 0 and len(self.new_folder_name) > 0:
+            # renaming file
+            old_name = self.absolute_path
+            new_name = os.path.join(self.path, self.new_file_name)
+            if old_name != new_name:
+                print(f'Renaming {self.key} to {self.new_file_name}')
+                os.rename(old_name, new_name)
+            # renaming folder
+            old_name = self.path
+            parent = str(Path(self.path).parent.absolute())
+            new_name = os.path.join(parent, self.new_folder_name)
+            if old_name != new_name:
+                print(f'Renaming {self.key} to {self.new_folder_name}')
+                os.rename(old_name, new_name)
+
 
 def movie_details(file, path, r = 0):
     temp = text_after_year(file)
@@ -40,7 +56,6 @@ def movie_details(file, path, r = 0):
     if movie['kind'] == 'movie':
         GLOBAL_MOVIES[file] = movie_struct(key = file, title = movie, year = movie['year'], path = path, recurse = r)
     else:
-        print("not a movie")
         movie_details(file = file, path = path, r = (r + 1))
 
 
@@ -88,19 +103,31 @@ def fix_movie_file():
 
             if file.endswith(".mp4") or file.endswith(".mkv"):
                 movie_details(file = file, path = subdir)
+def create_new_names(key):
+    title = GLOBAL_MOVIES[key].title
+    year = GLOBAL_MOVIES[key].year
+    ext = Path(GLOBAL_MOVIES[key].absolute_path).suffix
+    GLOBAL_MOVIES[key].new_file_name = f'{title} ({year}){ext}'
+    GLOBAL_MOVIES[key].new_folder_name = str(title)
+
+def check_redundancy(key):
+    movie = GLOBAL_MOVIES[key]
+    new_name = os.path.join(movie.path, movie.new_file_name)
+    old_name = os.path.join(movie.path, movie.key)
+    if new_name == old_name:
+        print('Already Renamed Properly')
+        return True
+    return False
+
+
 
 def validate(key):
-    if not GLOBAL_MOVIES[key].failed:
+    create_new_names(key)
+    if not GLOBAL_MOVIES[key].failed and not check_redundancy(key):
         print("Is this information correct?")
         GLOBAL_MOVIES[key].print()
         res = input()
-        if res == '': # the information is correct
-            title = GLOBAL_MOVIES[key].title
-            year = GLOBAL_MOVIES[key].year
-            ext = pathlib.Path(GLOBAL_MOVIES[key].absolute_path).suffix
-            GLOBAL_MOVIES[key].new_file_name = f'{title} ({year}){ext}'
-            GLOBAL_MOVIES[key].new_folder_name = title
-        else:
+        if res != '': # the information is correct
             movie_details(file = key, path = GLOBAL_MOVIES[key].path, r = GLOBAL_MOVIES[key].recurse + 1)
             if key in GLOBAL_MOVIES:
                 validate(key)
@@ -121,8 +148,7 @@ def main():
     for key in GLOBAL_MOVIES:
         new_file_name = GLOBAL_MOVIES[key].new_file_name
         new_folder_name = GLOBAL_MOVIES[key].new_folder_name
-        print(f'RENAMING File: {key} to {new_file_name}')
-        print(f'RENAMING Folder : {key} to {new_folder_name}')
+        GLOBAL_MOVIES[key].rename()
 
 
 
