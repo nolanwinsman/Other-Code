76632edc49e1c4335dbffb72074fbb0e66202452
nolanwinsman <nolanwinsman@gmail.com>
2025-01-04 20:32:11 -0800

    header comments added

--- python/copy_gc_games_to_wii_sd/main.py
+++ python/copy_gc_games_to_wii_sd/main.py
@@ -1,8 +1,17 @@
+#!/usr/bin/env python
+"""Script to automate copying Gamecube files to Wii SD card in correct Nintendont Format
+
+Wii Gamecube games need to be in the format
+<Gamecube Game Name>/game.iso
+
+https://sites.google.com/site/completesg/backup-launchers/gamecube/nintendont
+"""
+
 import os
 import shutil
 from pathlib import Path
 
-
+# Folder Paths for Gamecube Games folder and Wii Gamecube Games folder
 GAMECUBE_FOLDER = r"E:\Games"
 WII_GC_FOLDER = r"F:\Games"
 
@@ -27,14 +36,6 @@ def main():
                     wii_game = os.path.join(new_wii_folder, 'game.iso')
                     print(f"Copying and Renaming {gc_game} to {wii_game}")
                     shutil.copy2(gc_game, wii_game)
-
-
-
-                    
-
-
-
-
 
 if __name__ == '__main__':
     main()
